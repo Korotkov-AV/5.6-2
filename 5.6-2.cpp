9399fafa47e515f643c64c89683fed352919aab9
Alex <korotkov.aw@gmail.com>
2023-10-12 00:17:44 +0500

    Добавьте файлы проекта.

---
+++ 5.6-2.cpp
@@ -0,0 +1,23 @@
+﻿// 5.6-2.cpp 
+
+#include <iostream>
+#include <set>
+
+int main() {
+	int count_input_n, num;
+	std::set<int> set;
+
+	std::cin >> count_input_n;
+
+	while (count_input_n) {
+		std::cin >> num;
+		set.insert(num);
+
+		count_input_n--;
+	}
+
+	std::cout << "reverse:\n";
+	for (auto iter = set.rbegin(); iter != set.rend(); ++iter) {
+		std::cout << *iter << std::endl;
+	}
+}
